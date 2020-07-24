a61cacc23e7649550462a9460a6b100f2e7aa71d
Andres Gonzalez Gongora <30781086+Zappandy@users.noreply.github.com>
2020-07-23 23:40:11 -0500

    Update Web_Scrapper.py

--- Web_Scrapper.py
+++ Web_Scrapper.py
@@ -1,4 +1,9 @@
+import selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 Path = "Selenium Drivers/Edge_Driver/msedgedriver.exe"
 home_page = "https://www.asn-online.org/"
@@ -16,7 +21,14 @@ def main_func(address, signin_info):
     """
     driver = webdriver.Edge(Path)
     driver.get(address)
-    driver.find_element_by_id("login_link").click()
+    element_present = EC.presence_of_element_located((By.ID, "login_link"))
+    try:
+        login = WebDriverWait(driver, 10).until(element_present)
+    except selenium.common.exceptions.NoSuchElementException:
+        driver.quit()
+        return 1
+    #loginElm = driver.find_element_by_id("login_link")
+    login.click()
     username(driver, signin_info[0])
     password_info(driver, signin_info[1])
     member_dir(driver, "Torrance", "CA")
@@ -26,10 +38,14 @@ def username(web_browser, user):
     """
     Submits username data
     """
-    web_browser.implicitly_wait(1)
-    userElm = web_browser.find_element_by_id("login_eml_address")
-    userElm.send_keys(user)
-    userElm.submit()
+    element_present = EC.presence_of_element_located((By.ID, "login_eml_address"))
+    try:
+        userElm = WebDriverWait(web_browser, 10).until(element_present)
+        userElm.send_keys(user)
+        userElm.submit()
+    except selenium.common.exceptions.NoSuchElementException:
+        web_browser.quit()
+    #userElm = web_browser.find_element_by_id("login_eml_address")
 
 
 def password_info(web_browser, password):
@@ -49,18 +65,13 @@ def member_dir(web_browser, city, state):
     :return:
     """
     doctor_xpath = "/html/body/div[1]/section[2]/div/div/div[2]/div/form/div[3]/ul/li[1]"
-    web_browser.implicitly_wait(1)
     web_browser.find_element_by_id("membership").click()
-    web_browser.implicitly_wait(1)
     web_browser.find_element_by_link_text('Member Directory').click()
-    web_browser.implicitly_wait(1)
     cityElm = web_browser.find_element_by_id("THE_CITY")
     cityElm.send_keys(city)
-    web_browser.implicitly_wait(1)
     web_browser.find_element_by_xpath(f"//select[@name='THE_STATE']/option[text()='{state}']").click()
     web_browser.implicitly_wait(1)
     web_browser.find_element_by_name("search_button").click()
-    web_browser.implicitly_wait(1)
     result_list = web_browser.find_element_by_class_name("list1")
     doctors = result_list.find_elements_by_css_selector("li a")
     doc_data = []
@@ -68,11 +79,10 @@ def member_dir(web_browser, city, state):
         doc.click()
         #href = doc.get_attribute("href")
         #web_browser.execute_script("window.open('" + href +"');")
-        web_browser.implicitly_wait(10)
         #data_list = web_browser.find_element_by_id("member_panel")
         htmlElem = web_browser.find_element_by_xpath(doctor_xpath)
-        web_browser.implicitly_wait(10)
-        doc_data.append(htmlElem.get_attribute("innerHtml"))
+        doc_data.append(htmlElem)
+        #doc_data.append(htmlElem.get_attribute("innerHtml"))
         web_browser.implicitly_wait(1)
 
     print(doc_data)
