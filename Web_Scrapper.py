407a98e010e859eee7276e7cbdb065f8d42f223d
Andres Gonzalez Gongora <30781086+Zappandy@users.noreply.github.com>
2020-07-25 15:53:30 -0500

    Update Web_Scrapper.py

--- Web_Scrapper.py
+++ Web_Scrapper.py
@@ -5,91 +5,83 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-Path = "Selenium Drivers/Edge_Driver/msedgedriver.exe"
-home_page = "https://www.asn-online.org/"
 user_data = open("./Selenium Drivers/account_data.txt")
 access_data = [w[:-1] for w in user_data.readlines()]  # cleaning up new line char
 user_data.close()
 
 
-def main_func(address, signin_info):
-    """
+class Farmacon_Scrapper(object):
+    Path = "Selenium Drivers/Edge_Driver/msedgedriver.exe"    
 
-    :param signin_info:
-    :param address:
-    :return:
-    """
-    driver = webdriver.Edge(Path)
-    driver.get(address)
-    element_present = EC.presence_of_element_located((By.ID, "login_link"))
-    try:
-        login = WebDriverWait(driver, 10).until(element_present)
-    except selenium.common.exceptions.NoSuchElementException:
-        driver.quit()
-        return 1
-    #loginElm = driver.find_element_by_id("login_link")
-    login.click()
-    username(driver, signin_info[0])
-    password_info(driver, signin_info[1])
-    member_dir(driver, "Torrance", "CA")
+    def __init__(self, sigin_info):
+        self.driver = webdriver.Edge(Farmacon_Scrapper.Path) 
+        self.sigin_info = sigin_info
+        self.no_element = selenium.common.exceptions.NoSuchElementException
+        self.driver.get("https://www.asn-online.org")
 
+    def wait_page(self, element_tup, iterable=False):
+        if not iterable:
+            element_present = EC.presence_of_element_located(element_tup)
+        else:
+            element_present = EC.presence_of_all_elements_located(element_tup)
+        try:
+            Elm = WebDriverWait(self.driver, 10).until(element_present)
+            return Elm
+        except self.no_element as error:
+            self.driver.quit()
+            print(f"{element_tup[1]} not found\nError:{error}")
+            return 1
 
-def username(web_browser, user):
-    """
-    Submits username data
-    """
-    element_present = EC.presence_of_element_located((By.ID, "login_eml_address"))
-    try:
-        userElm = WebDriverWait(web_browser, 10).until(element_present)
+    def login_link(self):
+        self.wait_page((By.ID, "login_link")).click()
+
+    def username(self):
+        userElm = self.wait_page((By.ID, "login_eml_address"))
+        user = self.sigin_info[0]
         userElm.send_keys(user)
         userElm.submit()
-    except selenium.common.exceptions.NoSuchElementException:
-        web_browser.quit()
-    #userElm = web_browser.find_element_by_id("login_eml_address")
+
+    def password(self): 
+        passElm = self.wait_page((By.CLASS_NAME, "toggle-password-input"))
+        passwrd = self.sigin_info[1]
+        passElm.send_keys(passwrd)
+        self.wait_page((By.ID, "LoginButton")).click()
+
+    def member_dir(self, city, state):
+        stateDropdown_xpath = f"//select[@name='THE_STATE']/option[text()='{state}']"
+        self.wait_page((By.ID, "membership")).click()
+        self.wait_page((By.LINK_TEXT, "Member Directory")).click()
+        cityElm = self.wait_page((By.ID, "THE_CITY"))
+        cityElm.send_keys(city)
+        self.wait_page((By.XPATH, stateDropdown_xpath)).click()
+        self.wait_page((By.NAME, "search_button")).click()
+        result_list = self.wait_page((By.CLASS_NAME, "list1"))
+        return result_list.find_elements_by_css_selector("li a")
+
+    def member_data(self):
+        doctor_list = self.member_dir("Torrance", "CA")
+        doc_dataXPATH = "//ul[@class='list1 grey']/li[1]"#  "//*[@id='member_panel']/ul/li[1]"
+        #doctor_listXPATH = "//*[@id='results_panel']/ul"
+        for e, doc in enumerate(doctor_list):
+            href = doc.get_attribute("href")
+            # possible issues
+            self.driver.execute_script("window.open('');")
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            self.driver.get(href)
+            doc_Elm = self.wait_page((By.XPATH, doc_dataXPATH), iterable=True)
+            member_data = [li for li in doc_Elm]
+            self.driver.close()
+            self.driver.switch_to.window(self.driver.window_handles[0])
+            print(member_data)
+        #return raw_data
 
 
-def password_info(web_browser, password):
-    """
-    Submits password data
-    """
-    passElm = web_browser.find_element_by_class_name("toggle-password-input")
-    passElm.send_keys(password)
-    web_browser.implicitly_wait(1)
-    web_browser.find_element_by_id("LoginButton").click()
+def test(scrapper, signin_info):
+    asn = scrapper(signin_info)
+    asn.login_link()
+    asn.username()
+    asn.password()  # access_data
+    asn.member_data()
 
-
-def member_dir(web_browser, city, state):
-    """
-
-    :param web_browser:
-    :return:
-    """
-    doctor_xpath = "/html/body/div[1]/section[2]/div/div/div[2]/div/form/div[3]/ul/li[1]"
-    web_browser.find_element_by_id("membership").click()
-    web_browser.find_element_by_link_text('Member Directory').click()
-    cityElm = web_browser.find_element_by_id("THE_CITY")
-    cityElm.send_keys(city)
-    web_browser.find_element_by_xpath(f"//select[@name='THE_STATE']/option[text()='{state}']").click()
-    web_browser.implicitly_wait(1)
-    web_browser.find_element_by_name("search_button").click()
-    result_list = web_browser.find_element_by_class_name("list1")
-    doctors = result_list.find_elements_by_css_selector("li a")
-    doc_data = []
-    for doc in doctors:
-        doc.click()
-        #href = doc.get_attribute("href")
-        #web_browser.execute_script("window.open('" + href +"');")
-        #data_list = web_browser.find_element_by_id("member_panel")
-        htmlElem = web_browser.find_element_by_xpath(doctor_xpath)
-        doc_data.append(htmlElem)
-        #doc_data.append(htmlElem.get_attribute("innerHtml"))
-        web_browser.implicitly_wait(1)
-
-    print(doc_data)
-    return doc_data
-
-
-
-
-main_func(home_page, access_data)
+test(Farmacon_Scrapper, access_data)
 
